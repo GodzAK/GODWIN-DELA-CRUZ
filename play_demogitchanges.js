b386b57f766ea80c017f019f84942261ea506eba
GodzAK <2000188@ub.edu.ph>
2024-09-07 11:01:31 +0800

    1

---
+++ play_demogitchanges.js
@@ -0,0 +1,21 @@
+console.log("Demo Git Change");
+
+//add commit and push
+/*
+//1.create  new respirotory on github
+//2. go to the folder where your project is located
+//3.run git init
+//4.run git add
+//5. run git commit -m "Intial Commit"
+//6. Run git branch -m main
+//7.run git remote add origin <url>
+//8. run git push -u origin main
+
+*/
+
+// add edit, commit and push
+/*
+1. git add . 
+2.git commit -m "demo it changes"
+3. git push
+*/
